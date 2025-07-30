a596ff139c4774e6a54a46765d51a6d71515e999
attacker250 <104977532+attacker250@users.noreply.github.com>
2025-07-30 15:21:01 +0800

    fix

--- Main.js
+++ Main.js
@@ -287,16 +287,19 @@ function markerMove(event){
 
 function drag(event){
     document.getElementById("testres").innerHTML = "moving"
-    console.log(event,"event")
-    console.log(event.touches[0].clientX,"PageX")
+    // console.log(event,"event")
+    // console.log(event.touches[0].clientX,"PageX")
     if(mousedown == true && target != null && target.alt == "Marker"){
         var width = target.getBoundingClientRect().right - target.getBoundingClientRect().left
         var height = target.getBoundingClientRect().bottom - target.getBoundingClientRect().top
-        if(window.innerWidth >= 800){ //if page width is less than 800px
+        if(window.innerWidth <= 800){ //if page width is less than 800px
+            console.log("mobile")
             target.style.left = event.touches[0].clientX - target.parentElement.getBoundingClientRect().x - width/2+ "px"
-            target.style.top = event.touches[0].clientY - target.parentElement.getBoundingClientRect().y - height/2 + "px"        
+            target.style.top = event.touches[0].clientY - target.parentElement.getBoundingClientRect().y - height/2 + "px"    
+
         }
         else{
+            console.log("desktop")
             target.style.left = event.clientX - target.parentElement.getBoundingClientRect().x - width/2+ "px"
             target.style.top = event.clientY - target.parentElement.getBoundingClientRect().y - height/2 + "px"
         }
